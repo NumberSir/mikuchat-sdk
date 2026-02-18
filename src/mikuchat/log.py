6443d4a662548d8e80db5b9d592bebf682992793
Number_Sir <number_sir@126.com>
2026-02-18 14:24:12 +0800

    fix(log): 避免 logger 冲突

--- src/mikuchat/log.py
+++ src/mikuchat/log.py
@@ -1,9 +1,19 @@
-from loguru import logger as logger_
 import sys
 
-logger_.remove()
-logger_.add(sys.stdout, format="<g>{time:HH:mm:ss}</g> | [<lvl>{level}</lvl>] | {message}", colorize=True)
+from loguru import logger as logger_
 
-logger = logger_
+
+def _filter(record):
+    return record["extra"].get("name") == "MikuChatApi"
+
+
+logger_.add(
+    sys.stdout,
+    format="<g>{time:HH:mm:ss}</g> | [<lvl>{level}</lvl>] | {message}",
+    colorize=True,
+    filter=_filter
+)
+
+logger = logger_.bind(name="MikuChatApi")
 
 __all__ = ["logger"]
